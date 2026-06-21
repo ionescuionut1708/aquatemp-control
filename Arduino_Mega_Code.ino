80c6048bbed00d80b6a072c33a9ec1fab676770e
Ionut <ionut.ionescu1708@stud.acs.upb.ro>
2026-06-21 05:10:58 +0300

    Update Arduino_Mega_Code.ino

--- Arduino_Mega_Code.ino
+++ Arduino_Mega_Code.ino
@@ -15,7 +15,7 @@ DallasTemperature sensors(&oneWire);
 String inputString = "";
 boolean stringComplete = false;
 
-float setpoint = 25.0; // Valoare implicită [grade Celsius]
+float setpoint = 25.0; //implicit value [celsius degrees]
 float u_de_k = 0;
 float epsilon_de_k = 0;
 float epsilon_de_k_minus_1 = 0;
@@ -34,9 +34,8 @@ void setup() {
 void loop() {
   digitalWrite(R_EN_PIN, HIGH);
   digitalWrite(L_EN_PIN, HIGH);
-
   sensors.requestTemperatures();
-  
+
   while (Serial1.available()) {
     char inChar = (char)Serial1.read();
     inputString += inChar;
@@ -54,8 +53,8 @@ void loop() {
   }
 
   float temperature = sensors.getTempCByIndex(0);
-  
-  // Implementarea algoritmului PI
+
+  // Implementation of the PI algorithm
   epsilon_de_k = setpoint - temperature;
 
   if (epsilon_de_k < 0) {
@@ -64,30 +63,30 @@ void loop() {
     u_de_k = epsilon_de_k * 1.49 - epsilon_de_k_minus_1 * 1.29 + u_de_k_minus_1;
   }
 
-  if(u_de_k > 160) u_de_k = 160; //limitare pentru a nu deterioara elementul de incalzirea apei
+  if (u_de_k > 160) u_de_k = 160;  // limit to avoid damaging the water heating element
   else if (u_de_k < 0) u_de_k = 0;
 
   Serial.println(u_de_k);
   Serial.println("\n");
 
   analogWrite(RPWM_PIN, u_de_k);
+
   epsilon_de_k_minus_1 = epsilon_de_k;
   u_de_k_minus_1 = u_de_k;
-  
+
   int rawTDSValue = analogRead(TDS_SENSOR_PIN);
   float calibratedTDSValue = map(rawTDSValue, 0, CALIBRATION_VALUE, 0, 500);
 
   float temperaturePercentage = (temperature / setpoint) * 100;
   temperaturePercentage = constrain(temperaturePercentage, 0, 100);
 
-  // Modificăm formatul datelor trimise către ESP8266
   Serial1.print(temperature);
   Serial1.print(",");
   Serial1.print(calibratedTDSValue);
   Serial1.print(",");
   Serial1.print(temperaturePercentage);
   Serial1.print(",");
-  Serial1.println(u_de_k);  // Adăugăm valoarea PWM (u_de_k) la datele trimise
-  
+  Serial1.println(u_de_k);
+
   delay(500);
 }
